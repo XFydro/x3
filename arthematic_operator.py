7f3495bb5c1f8171c91679f23bdcc74a0576b3bc
XFydro <122276552+XFydro@users.noreply.github.com>
2025-03-01 15:38:21 +0530

    file marked for deletion

--- arthematic_operator.py
+++ arthematic_operator.py
@@ -1,7 +1,1 @@
-def scan_for_operators(input):
-    x=1
-    while (x<int(input)):
-        print("test")
-        x = x + 1
-
-scan_for_operators(input("repeat"))        
+I have no idea what even is this. 
